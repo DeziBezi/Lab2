cecea434bedd17dfb25b19f2b94da56ac2a4f072
Desiree Bailey <dpbailey@albany.edu>
2024-10-08 22:16:31 -0400

    Add files via upload

---
+++ Bailey_Desiree Lab1.py
@@ -0,0 +1,25 @@
+name = "Desiree"
+print(f"Hi my name is {name}, this is my print statement #1")
+
+
+
+print(f"Hi my name is {name}, this is my print statement #2")
+          
+
+
+print(f"Hi my name is {name}, this is my print statement #3")
+
+num_days = 720
+
+print(f"{num_days} is the integer value of the number of days in september")
+
+dividend = 101
+divisor = 4
+
+quotient = dividend // divisor
+
+remainder = dividend % divisor
+
+print("quotient(rounded)", quotient)
+print("remainder(modulo)", remainder)
+
